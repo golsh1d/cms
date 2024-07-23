75b4fc56293e29a3228a1afd998d22b906d7c21a
golshid <golsheedd@gmail.com>
2024-07-23 17:20:52 +0330

    add overlay codes

--- frontend/content/scripts/app.js
+++ frontend/content/scripts/app.js
@@ -2,19 +2,22 @@ let bars = document.querySelector('.fa-bars-staggered')
 let aside = document.querySelector('aside')
 let main = document.querySelector('main')
 let mainContent = document.querySelector('.main-content')
+let overlay = document.querySelector('.overlay')
 
 function showSideNav() {
     aside.style.transform = 'translateX(0px)'
     aside.style.transition = 'all 0.2s ease'
+    overlay.style.display = 'block'
 }
 
 
 function hideSideNav() {
     aside.style.transform = 'translateX(250px)'
     aside.style.transition = 'all 0.2s ease'
+    overlay.style.display = 'none'
 }
 
 
 // events
 bars.addEventListener('click' , showSideNav)
-mainContent.addEventListener('click' , hideSideNav)
+overlay.addEventListener('click' , hideSideNav)
