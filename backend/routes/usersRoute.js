1a699d662bb335f9dfb2749d361cbb6a48c80b46
golshid <golsheedd@gmail.com>
2024-07-27 10:07:51 +0330

    add update user api

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -24,4 +24,17 @@ usersRoute.delete('/:userId' , (req , res) => {
         }
     })
 })
+
+usersRoute.put('/:userId' , (res , req) => {
+    let userId = req.params.userId
+    let body = req.body
+    let updateUserQuery = `UPDATE users SET firstName=${body.firstName},lastName=${body.lastName},userName=${body.userName},password=${body.password},phone=${body.phone},email=${body.email},city=${body.city},address=${body.address},score=${body.score},buy=${body.buy} WHERE id = ${userId}`
+    sqlConnection.query(updateUserQuery , (err , result) => {
+        if (err) {
+            res.send(null)
+        } else {
+            res.send(result)
+        }
+    })
+})
 module.exports = usersRoute
