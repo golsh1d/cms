72c4021a05f38c49068d75a199b580ef3c2d9f28
golshid <golsheedd@gmail.com>
2024-07-27 10:49:30 +0330

    add select all orders api

--- backend/routes/ordersRoute.js
+++ backend/routes/ordersRoute.js
@@ -1,5 +1,16 @@
 let express = require('express')
-
+let sqlConnection = require('./../DB/cmsShop')
 let ordersRoute = express.Router()
+
+ordersRoute.get('/' , (req , res) => {
+    let selectAllOrdersQuery = `SELECT * FROM orders`
+    sqlConnection.query(selectAllOrdersQuery , (err , result) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 module.exports = ordersRoute
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,17 @@ let express = require('express')
 let cors = require('cors')
 let bodyParser = require('body-parser')
 let productsRouter = require('./routes/productsRoute')
+let commentsRouter = require('./routes/commentsRoute')
+let usersRouter = require('./routes/usersRoute')
+let ordersRouter = require('./routes/ordersRoute')
 
 let app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api/products' , productsRouter)
+app.use('/api/comments' , commentsRouter)
+app.use('/api/users' , usersRouter)
+app.use('/api/orders' , ordersRouter)
 
 app.listen(3000)
