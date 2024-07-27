ae7f28739528ae0dadcb92c6c2fa86fd251a00cf
golshid <golsheedd@gmail.com>
2024-07-27 09:38:09 +0330

    add select all comments api

--- backend/routes/commentsRoute.js
+++ backend/routes/commentsRoute.js
@@ -1,5 +1,16 @@
 let express = require('express')
-
+let sqlConnection = require('./../DB/cmsShop')
 let commentsRoute = express.Router()
+
+commentsRoute.get('/', (req , res) => {
+    let selectAllCommentsQuery = `SELECT * FROM comments`
+    sqlConnection.query(selectAllCommentsQuery , (err , result) => {
+        if (err) {
+            res.send(null)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 module.exports = commentsRoute
