d5f436b162f906582ed837dd8e68e7900bcac499
golshid <golsheedd@gmail.com>
2024-07-27 10:01:37 +0330

    add delete user api

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -13,4 +13,15 @@ usersRoute.get('/' , (req , res) => {
     })
 })
 
+usersRoute.delete('/:userId' , (req , res) => {
+    let userId = req.params.userId
+    let deleteUserQuery = `DELETE FROM users WHERE id = ${userId}`
+    sqlConnection.query(deleteUserQuery , (err , result) => {
+        if (err) {
+            res.send(null)
+        } else {
+            res.send(result)
+        }
+    })
+})
 module.exports = usersRoute
