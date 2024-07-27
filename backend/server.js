1451533a37aa8703e2721dcfe94cce5fe689c265
golshid <golsheedd@gmail.com>
2024-07-27 09:25:18 +0330

    use products api in server js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 let express = require('express')
 let cors = require('cors')
 let bodyParser = require('body-parser')
+let productsRouter = require('./routes/productsRoute')
 
 let app = express()
 app.use(cors())
 app.use(bodyParser.json())
+
+app.use('/api/products' , productsRouter)
 
 app.listen(3000)
