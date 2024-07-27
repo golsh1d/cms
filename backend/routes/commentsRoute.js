4d565169b09c3a650a543e1088f9b3c4ab154fff
golshid <golsheedd@gmail.com>
2024-07-27 09:43:56 +0330

    add delete comment api

--- backend/routes/commentsRoute.js
+++ backend/routes/commentsRoute.js
@@ -1,10 +1,23 @@
 let express = require('express')
 let sqlConnection = require('./../DB/cmsShop')
+const { result } = require('lodash')
 let commentsRoute = express.Router()
 
 commentsRoute.get('/', (req , res) => {
     let selectAllCommentsQuery = `SELECT * FROM comments`
     sqlConnection.query(selectAllCommentsQuery , (err , result) => {
+        if (err) {
+            res.send(null)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
+commentsRoute.delete('/:commentId' , (req , res) => {
+    let commentId = req.params.commentId
+    let deleteCommentQuery = `DELETE FROM comments WHERE id = ${commentId}`
+    sqlConnection.query(deleteCommentQuery , (err , result) => {
         if (err) {
             res.send(null)
         } else {
