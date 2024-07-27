7a5416c4a1f78fc11ce815c373d04ae128a31dc4
golshid <golsheedd@gmail.com>
2024-07-27 08:41:18 +0330

    add select all products query api

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,5 +1,16 @@
 let express = require('express')
-
+let sqlConnection = require('./../DB/cmsShop.js')
 let productsRoute = express.Router()
+
+productsRoute.get('/' , (req , res) => {
+    let selectAllProductsQuery = `SELECT * FROM products`
+    sqlConnection.query(selectAllProductsQuery , (err , result) => {
+        if (err) {
+            res.send(null)
+        } else {
+            res.send(result)
+        }
+    })
+})
 
 module.exports = productsRoute
