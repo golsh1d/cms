4366aed8b4873178793997e4fd05139cb0f86816
golshid <golsheedd@gmail.com>
2024-07-27 11:59:40 +0330

    add is active api

--- backend/routes/ordersRoute.js
+++ backend/routes/ordersRoute.js
@@ -25,4 +25,17 @@ ordersRoute.delete('/:orderId' , (req , res) => {
     })
 })
 
+ordersRoute.put('/active-order/:orderId/:isActive' , (req , res) => {
+    let orderId = req.params.orderId
+    let isActive = req.params.isActive
+    let isActiveQuery = `UPDATE orders SET isActive=${isActive} WHERE id = ${orderId}`
+    sqlConnection.query(isActiveQuery , (err , result) => {
+        if (err) {
+            res.send(null)
+        } else {
+            res.send(result)
+        }
+    })
+})
+
 module.exports = ordersRoute
