440a20b41095204058110fec950b1a9561c0dc69
golshid <golsheedd@gmail.com>
2024-07-28 11:21:56 +0330

    cheched users apis with postman

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -25,10 +25,10 @@ usersRoute.delete('/:userId' , (req , res) => {
     })
 })
 
-usersRoute.put('/:userId' , (res , req) => {
+usersRoute.put('/:userId' , (req , res) => {
     let userId = req.params.userId
     let body = req.body
-    let updateUserQuery = `UPDATE users SET firstName=${body.firstName},lastName=${body.lastName},userName=${body.userName},password=${body.password},phone=${body.phone},email=${body.email},city=${body.city},address=${body.address},score=${body.score},buy=${body.buy} WHERE id = ${userId}`
+    let updateUserQuery = `UPDATE users SET firstName='${body.firstName}',lastName='${body.lastName}',userName='${body.userName}',password='${body.password}',phone=${body.phone},email='${body.email}',city='${body.city}',address='${body.address}',score=${body.score},buy=${body.buy} WHERE id = ${userId}`
     sqlConnection.query(updateUserQuery , (err , result) => {
         if (err) {
             res.send(null)
@@ -37,4 +37,5 @@ usersRoute.put('/:userId' , (res , req) => {
         }
     })
 })
+
 module.exports = usersRoute
