9d022c598ceb8d7582b5a4e23331c8026845737b
golshid <golsheedd@gmail.com>
2024-07-29 10:59:59 +0330

    add condition to show empty err box

--- frontend/content/scripts/product.js
+++ frontend/content/scripts/product.js
@@ -6,29 +6,34 @@ let updateModal = document.querySelector('.update-modal-wrapper')
 let deleteModal = document.querySelector('.delete-modal-wrapper')
 let productTableBody = document.querySelector('.product-table-body')
 let modalElem = document.querySelectorAll('.modal')
+let emptyErr = document.querySelector('.empty-err')
 
 function showAllProducts() {
     fetch('http://localhost:3000/api/products/')
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        data.forEach(obj => {
-            productTableBody.insertAdjacentHTML(`beforeend`,
-                `<tr class="cms-table-tr">
-                        <td class="xs-w-60 s-w-100 xs-hidden s-block product-table-tr-first-td">
-                            <img src="${obj.img}" class="cms-table-img" alt="">
-                        </td>
-                        <td class="xs-w-60 s-w-100 product-table-tr-second-td">${obj.title}</td>
-                        <td class="xs-w-60 s-w-100 product-table-tr-third-td">${obj.price}</td>
-                        <td class="xs-w-60 s-w-100 product-table-tr-forth-td">${obj.count}</td>
-                        <td class="xs-w-60 s-w-100 product-table-tr-fifth-td">
-                            <button onclick=showDetail(${obj.id}) class="cms-table-btn product-table-detail-btn">جزئیات</button>
-                            <button class="cms-table-btn product-table-delete-btn">حذف</button>
-                            <button class="cms-table-btn product-table-update-btn">ویرایش</button>
-                        </td>
-                    </tr>`
-            )
-        })
+        if (data.length) {
+            emptyErr.style.display = 'none'
+            data.forEach(obj => {
+                productTableBody.insertAdjacentHTML(`beforeend`,
+                    `<tr class="cms-table-tr">
+                            <td class="xs-w-60 s-w-100 xs-hidden s-block product-table-tr-first-td">
+                                <img src="${obj.img}" class="cms-table-img" alt="">
+                            </td>
+                            <td class="xs-w-60 s-w-100 product-table-tr-second-td">${obj.title}</td>
+                            <td class="xs-w-60 s-w-100 product-table-tr-third-td">${obj.price}تومان</td>
+                            <td class="xs-w-60 s-w-100 product-table-tr-forth-td">${obj.count}عدد</td>
+                            <td class="xs-w-60 s-w-100 product-table-tr-fifth-td">
+                                <button onclick=showDetail(${obj.id}) class="cms-table-btn product-table-detail-btn">جزئیات</button>
+                                <button class="cms-table-btn product-table-delete-btn">حذف</button>
+                                <button class="cms-table-btn product-table-update-btn">ویرایش</button>
+                            </td>
+                        </tr>`
+                )
+            })
+        } else {
+            emptyErr.style.display = 'block'
+        }
     })
 }
 
