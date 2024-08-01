827636499c8928f9a0d38629b9e50c87436c1a0b
golshid <golsheedd@gmail.com>
2024-08-01 13:17:17 +0330

    add codes to delete an off

--- frontend/content/scripts/offs.js
+++ frontend/content/scripts/offs.js
@@ -1,6 +1,9 @@
 let mainUrl = `http://localhost:3000/api/`
 let emptyErrBox = document.querySelector('.empty-err')
 let offsTableBody = document.querySelector('.offs-table-body')
+let deleteModalWrapper = document.querySelector('.delete-modal-wrapper')
+let deleteModalYesBtn = document.querySelector('.delete-modal-yes-btn')
+let deleteModalNoBtn = document.querySelector('.delete-modal-no-btn')
 
 function showAllOffs() {
     fetch(`${mainUrl}offs/`)
@@ -18,7 +21,7 @@ function showAllOffs() {
                         <td class="xs-w-50 s-w-60 md-w-100 xs-hidden s-block">${obj.adminId}</td>
                         <td class="xs-w-50 s-w-60 md-w-100">${obj.productId}</td>
                         <td class="offs-table-details xs-w-60 s-w-60 md-w-100">
-                            <button class="cms-table-btn">حذف</button>
+                            <button class="cms-table-btn" onclick="showDeleteModal(${obj.id})">حذف</button>
                             <button class="cms-table-btn">تایید</button>
                         </td>
                     </tr>`
@@ -31,10 +34,46 @@ function showAllOffs() {
     })
 }
 
+//show delete modal
+function showDeleteModal(id) {
+    deleteModalWrapper.classList.add('active')
+    deleteModalNoBtn.addEventListener('click' , () => {
+        deleteModalWrapper.classList.remove('active')
+        location.reload()
+    })
+    deleteModalYesBtn.addEventListener('click' , async () => {
+        try {
+            let res = await fetch(`${mainUrl}offs/${id}` , {
+                method : 'DELETE',
+                headers : {
+                    'Content-type' : 'application/json'
+                },
+            })
+            console.log(res)
+            deleteModalWrapper.classList.remove('active')
+            showAllOffs()
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
 
+//hide delete modal
+function hideDeleteModal(event) {
+    if (event.target.classList[1] === 'delete-modal-wrapper') {
+        deleteModalWrapper.classList.remove('active')
+        location.reload()
+    }
+}
 
-
-
+function hideDeleteModalwithKey(event) {
+    if(event.key === 'Escape') {
+        deleteModalWrapper.classList.remove('active')
+        location.reload()
+    }
+}
 
 //events
 window.addEventListener('load' , showAllOffs)
+window.addEventListener('click' , hideDeleteModal)
+window.addEventListener('keydown' , hideDeleteModalwithKey)
